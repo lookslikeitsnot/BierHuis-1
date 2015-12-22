458924d4d750c49ac9c3c29a2ba9f8f0a3f9c007
guillaume.vandecasteele <guillaume.vandecasteele@gmail.com>
2015-12-22 04:41:08 +0100

    Instead of just adding Bier to ModelAndView, I'm now adding a command object BestelbonLijn

--- src/main/java/be/vdab/web/BierController.java
+++ src/main/java/be/vdab/web/BierController.java
@@ -1,8 +1,7 @@
 package be.vdab.web;
 
+import be.vdab.valueobjects.BestelbonLijn;
 import be.vdab.entities.Bier;
-import be.vdab.services.BierService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,18 +13,11 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 @RequestMapping("/bieren")
-public class BierController {
+class BierController {
     private static final String BIER_VIEW = "bieren/bier";
-
-    private final BierService bierService;
-
-    @Autowired
-    public BierController(BierService bierService) {
-        this.bierService = bierService;
-    }
 
     @RequestMapping(value = "{bier}", method = RequestMethod.GET)
     ModelAndView getBier(@PathVariable Bier bier) {
-        return new ModelAndView(BIER_VIEW).addObject(bier);
+        return new ModelAndView(BIER_VIEW).addObject(new BestelbonLijn(bier));
     }
 }
