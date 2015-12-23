e705945570321586029e1c0d943183f6e56f0d20
guillaume.vandecasteele <guillaume.vandecasteele@gmail.com>
2015-12-23 01:04:05 +0100

    Created exception for use in controllers

---
+++ src/main/java/be/vdab/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package be.vdab.exceptions;
+
+/**
+ * Created on 23/12/2015 at 00:24.
+ */
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
