0425244c577bf39d9525998aa54e768500f8977e
guycastle <guillaume.vandecasteele@gmail.com>
2016-02-01 09:38:39 +0100

    Error is being redirected for every 404 now, overrode the createDispatcherServlet method in initializer so that I could call the setThrowExceptionIfNoHandlerFound() method

--- src/main/java/be/vdab/web/Initializer.java
+++ src/main/java/be/vdab/web/Initializer.java
@@ -7,7 +7,10 @@ import be.vdab.dao.CreateDAOBeans;
 import be.vdab.datasource.CreateDataSourceBean;
 import be.vdab.services.CreateServiceBeans;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.Filter;
@@ -33,5 +36,12 @@ public class Initializer extends AbstractAnnotationConfigDispatcherServletInitia
         CharacterEncodingFilter utf8Filter = new CharacterEncodingFilter();
         utf8Filter.setEncoding("UTF-8");
         return new Filter[]{utf8Filter, new OpenEntityManagerInViewFilter()};
+    }
+
+    @Override
+    protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(servletAppContext);
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        return dispatcherServlet;
     }
 }
